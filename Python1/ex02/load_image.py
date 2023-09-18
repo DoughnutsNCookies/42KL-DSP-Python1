6bf1df6d373279549d3ada873c5b56e60a225997
Chuah Tse Yung <105269435+DoughnutsNCookies@users.noreply.github.com>
2023-09-18 12:13:17 +0000

    ex02

--- Python1/ex02/load_image.py
+++ Python1/ex02/load_image.py
@@ -9,7 +9,8 @@ def ft_load(path: str) -> list:
     try:
         with Image.open(path) as img:
             pixel_data = list(img.getdata())
-            print(f"The shape of the image is: ({img.height}, {img.width}, {img.getbands()})")
+            print("The shape of the image is:" \
+                  f" ({img.height}, {img.width}, {len(img.getbands())})")
             ret_list = []
             for x in range(img.height):
                 ret_list.append(pixel_data[x * img.width:(x + 1) * img.width])
