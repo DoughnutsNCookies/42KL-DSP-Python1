421002bddf21c0326a52aeb706338a1f0d06c5f3
Chuah Tse Yung <105269435+DoughnutsNCookies@users.noreply.github.com>
2023-09-18 12:15:46 +0000

    ex03

---
+++ Python1/ex03/zoom.py
@@ -0,0 +1,70 @@
+from load_image import ft_load
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def ft_zoom(path: str, factor: int) -> list:
+    """
+        Zoom an image by a factor
+        Return a list of pixel data
+    """
+    if not isinstance(path, str):
+        print("Path must be a string")
+        return []
+
+    if not isinstance(factor, (int, float)):
+        print("Factor must be an integer or a float")
+        return []
+
+    if factor <= 0:
+        print("Factor must be greater than 0")
+        return []
+
+    pixel_data = ft_load(path)
+    print(pixel_data)
+    if factor == 1:
+        new_pixel_data = pixel_data
+        new_h, new_w = len(pixel_data), len(pixel_data[0])
+    else:
+        w, h = len(pixel_data[0]), len(pixel_data)
+        new_w = int(w / factor)
+        new_h = int(h / factor)
+        if factor < 1:
+            new_pixel_data = [(0, 0, 0)] * new_w
+            new_pixel_data = [new_pixel_data.copy() for _ in range(new_h)]
+
+            paste_x = (new_w - w) // 2
+            paste_y = (new_h - h) // 2
+
+            for y in range(h):
+                for x in range(w):
+                    new_pixel_data[y + paste_y][x + paste_x] = pixel_data[y][x]
+        else:
+            x1 = (w - new_w) // 2
+            x2 = x1 + new_w
+            y1 = (h - new_h) // 2
+            y2 = y1 + new_h
+
+            new_pixel_data = [row[x1:x2] for row in pixel_data[y1:y2]]
+
+    print(f"New shape after slicing: ({new_h}, {new_w}, 3)")
+    print(new_pixel_data)
+    image_array = np.array(new_pixel_data, dtype=np.uint8)
+
+    plt.imshow(image_array)
+    plt.savefig("zoom.png")  # plt.show()
+    return new_pixel_data
+
+
+def main():
+    """
+        Main function
+    """
+    ft_zoom("animal.jpeg", 2)
+    # ft_zoom("animal.jpeg", 1)
+    # ft_zoom("animal.jpeg", 0)
+    # ft_zoom("animal.jpeg", 0.5)
+
+
+if __name__ == "__main__":
+    main()
