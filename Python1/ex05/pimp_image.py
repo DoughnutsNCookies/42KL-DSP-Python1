aff4624a9835b782ad0a5d5e8cddccb6116f5ec2
Chuah Tse Yung <105269435+DoughnutsNCookies@users.noreply.github.com>
2023-09-18 14:21:43 +0000

    ex05

---
+++ Python1/ex05/pimp_image.py
@@ -0,0 +1,93 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def ft_invert(array) -> list:
+    """
+        Invert an image
+        Return a list of pixel data
+    """
+    inverted_pixel_value = [
+        [tuple(255 - value for value in pixel) for pixel in row]
+        for row in array
+    ]
+
+    print("Inverted pixel value:")
+    print(inverted_pixel_value)
+
+    image_array = np.array(inverted_pixel_value, dtype=np.uint8)
+    plt.imshow(image_array)
+    plt.savefig("invert.png")
+    return inverted_pixel_value
+
+
+def ft_color(array, color: str) -> list:
+    """
+        Colors an image
+        Return a list of pixel data
+    """
+    pixel_values = []
+
+    color_map = {
+        "red": 0,
+        "green": 1,
+        "blue": 2
+    }
+
+    pixel_values = [[tuple(pixel[i] if i == color_map[color] else 0
+                    for i in range(3))
+                    for pixel in row]
+                    for row in array]
+
+    print(color.capitalize() + " pixel value:")
+    print(pixel_values)
+
+    image_array = np.array(pixel_values, dtype=np.uint8)
+    plt.imshow(image_array)
+    plt.savefig(color + ".png")
+    return pixel_values
+
+
+def ft_red(array) -> list:
+    """
+        Red an image
+        Return a list of pixel data
+    """
+    return ft_color(array, "red")
+
+
+def ft_green(array) -> list:
+    """
+        Green an image
+        Return a list of pixel data
+    """
+    return ft_color(array, "green")
+
+
+def ft_blue(array) -> list:
+    """
+        Blue an image
+        Return a list of pixel data
+    """
+    return ft_color(array, "blue")
+
+
+def ft_grey(array) -> list:
+    """
+        Grey an image
+        Return a list of pixel data
+    """
+    grey_pixel_values = []
+
+    grey_pixel_values = [
+        [(max(pixel[0], pixel[1], pixel[2]),) * 3 for pixel in row]
+        for row in array
+    ]
+
+    print("Grey pixel value:")
+    print(grey_pixel_values)
+
+    image_array = np.array(grey_pixel_values, dtype=np.uint8)
+    plt.imshow(image_array)
+    plt.savefig("grey.png")
+    return grey_pixel_values
---
+++ Python1/ex05/tester.py
@@ -0,0 +1,19 @@
+from load_image import ft_load
+from pimp_image import ft_invert, ft_red, ft_green, ft_blue, ft_grey
+
+def main():
+    """
+        Test main function
+    """
+    pixel_data = ft_load("landscape.jpeg")
+    print(pixel_data)
+
+    ft_invert(pixel_data)
+    ft_red(pixel_data)
+    ft_green(pixel_data)
+    ft_blue(pixel_data)
+    ft_grey(pixel_data)
+
+
+if __name__ == "__main__":
+    main()
