ed86e7a8ab4c9e666d8ec7a90c4e32126fea8726
Chuah Tse Yung <105269435+DoughnutsNCookies@users.noreply.github.com>
2023-09-18 05:58:49 +0000

    ex00

--- Python1/ex00/give_bmi.py
+++ Python1/ex00/give_bmi.py
@@ -5,6 +5,15 @@
 
 def give_bmi(height: list[int | float],
              weight: list[int | float]) -> list[int | float]:
+    """
+        Calculate BMI from height and weight
+
+        Raise ValueError if:
+            - Length of height and weight are not the same
+            - Height or weight is not a positive integer or float
+
+        Return a list of BMI
+    """
     if len(height) != len(weight):
         raise ValueError("Length of height and weight must be the same")
     for h in height:
@@ -18,6 +27,15 @@ def give_bmi(height: list[int | float],
 
 
 def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
+    """
+        Apply limit to BMI
+
+        Raise ValueError if:
+            - BMI is not a integer or float
+            - Limit is not a integer or float
+
+        Return a list of boolean
+    """
     for b in bmi:
         if not (isinstance(b, float) or isinstance(b, int)):
             raise ValueError("BMI must be a integer or float")
