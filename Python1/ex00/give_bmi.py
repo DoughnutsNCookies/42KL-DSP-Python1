461b0b5c4e4b887641fccc3be17da6a6927c2d97
Chuah Tse Yung <105269435+DoughnutsNCookies@users.noreply.github.com>
2023-09-19 08:24:55 +0000

    ex00

--- Python1/ex00/give_bmi.py
+++ Python1/ex00/give_bmi.py
@@ -9,26 +9,11 @@ def give_bmi(height: list[int | float],
 
         Return a list of BMI
     """
-    if height is None or weight is None:
-        print("Height and weight must not be None")
+    try:
+        return [w / (h * h) for h, w in zip(height, weight)]
+    except Exception as err:
+        print(err)
         return []
-    if not isinstance(height, list) or not isinstance(weight, list):
-        print("Height and weight must be a list")
-        return []
-    if len(height) != len(weight):
-        print("Length of height and weight must be the same")
-        return []
-    ret = []
-    for h, w in zip(height, weight):
-        if not isinstance(h, (float, int)) or not isinstance(w, (float, int)):
-            print("Height and eight must be a integer or float")
-            return []
-        if h <= 0 or w <= 0:
-            print("Height and weight must be a positive integer or float")
-            return []
-        ret.append(w / (h * h))
-        
-    return ret
 
 
 def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
@@ -41,19 +26,8 @@ def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
 
         Return a list of boolean
     """
-    if bmi is None:
-        print("BMI must not be None")
+    try:
+        return [b > limit for b in bmi]
+    except Exception as err:
+        print(err)
         return []
-    if not isinstance(bmi, list):
-        print("BMI must be a list")
-        return []
-    for b in bmi:
-        if not isinstance(b, (float, int)):
-            print("BMI must be a integer or float")
-            return []
-
-    if not isinstance(limit, (int, float)):
-        print("Limit must be a integer or float")
-        return []
-
-    return [b > limit for b in bmi]
