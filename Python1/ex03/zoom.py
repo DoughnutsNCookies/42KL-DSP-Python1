386c9cf8a073570ab599e1d8065f4fdb44ef24a6
Chuah Tse Yung <105269435+DoughnutsNCookies@users.noreply.github.com>
2023-09-19 10:06:24 +0000

    ex03

--- Python1/ex03/zoom.py
+++ Python1/ex03/zoom.py
@@ -3,65 +3,52 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def ft_zoom(path: str, factor: int) -> list:
+def ft_zoom(path: str, factor: float) -> np.ndarray:
     """
-        Zoom an image by a factor
-        Return a list of pixel data
+    Zoom an image by a factor using NumPy.
+    Return a NumPy array of pixel data.
     """
     try:
-        if not isinstance(factor, (int, float)):
-            raise TypeError("Factor must be an integer or a float")
-
-        if factor <= 0:
-            raise ValueError("Factor must be greater than 0")
-
         pixel_data = ft_load(path)
-        print(pixel_data)
-
         if factor == 1:
             new_pixel_data = pixel_data
-            new_h, new_w = len(pixel_data), len(pixel_data[0])
         else:
-            w, h = len(pixel_data[0]), len(pixel_data)
-            new_w = int(w / factor)
-            new_h = int(h / factor)
+            h, w, channels = pixel_data.shape
+
             if factor < 1:
-                new_pixel_data = [(0, 0, 0)] * new_w
-                new_pixel_data = [new_pixel_data.copy() for _ in range(new_h)]
+                new_h = int(h / factor)
+                new_w = int(w / factor)
+                print(new_h, new_w)
+                new_pixel_data = np.zeros((new_h, new_w, channels), dtype=np.uint8)
 
                 paste_x = (new_w - w) // 2
                 paste_y = (new_h - h) // 2
 
-                for y in range(h):
-                    for x in range(w):
-                        new_y = y + paste_y
-                        new_x = x + paste_x
-                        new_pixel_data[new_y][new_x] = pixel_data[y][x]
+                new_pixel_data[paste_y:paste_y + h, paste_x:paste_x + w] = pixel_data
             else:
-                x1 = (w - new_w) // 2
-                x2 = x1 + new_w
-                y1 = (h - new_h) // 2
-                y2 = y1 + new_h
+                x1 = (w - int(w / factor)) // 2
+                x2 = x1 + int(w / factor)
+                y1 = (h - int(h / factor)) // 2
+                y2 = y1 + int(h / factor)
 
-                new_pixel_data = [row[x1:x2] for row in pixel_data[y1:y2]]
+                new_pixel_data = pixel_data[y1:y2, x1:x2, :]
 
-        print(f"New shape after slicing: ({new_h}, {new_w}, 3)")
-        print(new_pixel_data)
-        image_array = np.array(new_pixel_data, dtype=np.uint8)
+        print(f"New shape after slicing: {new_pixel_data.shape}")
+        plt.imshow(new_pixel_data)
+        plt.savefig("zoom.png")
+        plt.show()
 
-        plt.imshow(image_array)
-        plt.savefig("zoom.png")  # plt.show()
         return new_pixel_data
     except Exception as err:
         print(err)
-        return []
+        return np.array([])
 
 
 def main():
     """
         Main function
     """
-    ft_zoom("animal.jpeg", 2)
+    # ft_zoom("animal.jpeg", 2)
     # ft_zoom("animal.jpeg", 1)
     # ft_zoom("animal.jpeg", 0)
     # ft_zoom("animal.jpeg", 0.5)
