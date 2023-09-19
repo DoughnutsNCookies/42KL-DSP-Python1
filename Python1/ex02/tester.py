fc6faa17ea118ee208c182ad6dae3c5f6f1c2ac6
Chuah Tse Yung <105269435+DoughnutsNCookies@users.noreply.github.com>
2023-09-19 08:45:59 +0000

    ex02

--- Python1/ex02/tester.py
+++ Python1/ex02/tester.py
@@ -1,8 +1,9 @@
 from load_image import ft_load
 
-# print(ft_load("landscape.jpeg"))
+print(ft_load("landscape.jpeg"))
 print(ft_load("landscape.jpg"))
 print(ft_load(None))
 print(ft_load(1))
 print(ft_load("a"))
 print(ft_load(["a"]))
+print(ft_load("noperm.jpg"))
